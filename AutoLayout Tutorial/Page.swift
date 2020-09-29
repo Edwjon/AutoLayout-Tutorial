90ab8ff13ab20cd35f3b2e04ab9e54cc8f542ba7
Edwjon <edwardpizzurro@gmail.com>
2020-09-29 01:21:16 -0400

    Commit

---
+++ AutoLayout Tutorial/Page.swift
@@ -0,0 +1,19 @@
+//
+//  Page.swift
+//  AutoLayout Tutorial
+//
+//  Created by Edward Pizzurro Fortun on 9/28/20.
+//  Copyright © 2020 Edwjon. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    
+    let imageName: String
+    let headerText: String
+    let bodyText: String
+    
+}
+
+
---
+++ AutoLayout Tutorial/SwipingController+extension.swift
@@ -0,0 +1,30 @@
+//
+//  SwipingController+extension.swift
+//  AutoLayout Tutorial
+//
+//  Created by Edward Pizzurro Fortun on 9/28/20.
+//  Copyright © 2020 Edwjon. All rights reserved.
+//
+
+import UIKit
+
+extension SwipingController {
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        coordinator.animate(alongsideTransition: { (_) in
+            self.collectionViewLayout.invalidateLayout()
+            
+            if self.pageControl.currentPage == 0 {
+                self.collectionView?.contentOffset = .zero
+            } else {
+                let indexPath = IndexPath(item: self.pageControl.currentPage, section: 0)
+                self.collectionView?.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            }
+            
+        }) { (_) in
+            
+        }
+    }
+    
+}
